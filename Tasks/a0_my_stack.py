083073bee75c9ece7bef3403c751961e24092f3f
EvgeniiRomanov <e78rus@mail.ru>
2022-02-12 08:06:05 +0000

    a0_my_stack.py - done

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -6,7 +6,7 @@ from typing import Any
 
 class Stack:
     def __init__(self):
-        ...  # todo для стека можно использовать python list
+        self.__my_stack = []  # todo для стека можно использовать python list
 
     def push(self, elem: Any) -> None:
         """
@@ -15,6 +15,7 @@ class Stack:
         :param elem: element to be pushed
         :return: Nothing
         """
+        self.__my_stack.append(elem)
         print(elem)
         return None
 
@@ -24,7 +25,11 @@ class Stack:
 
         :return: popped element
         """
-        return None
+        if len(self.__my_stack) == 0:
+            return None
+
+        return self.__my_stack.pop()
+
 
     def peek(self, ind: int = 0) -> Any:
         """
@@ -33,8 +38,18 @@ class Stack:
         :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
         :return: peeked element or None if no element in this place
         """
-        print(ind)
-        return None
+        #print(ind)
+        # 0 -> -1
+        # 1 -> -2
+        # 2 -> -3
+
+        # rev = self.__my_stack[::-1]   #сложность N
+        # return rev[ind]
+
+        if ind >= len(self.__my_stack):
+            return None
+        return self.__my_stack[-1 - ind]
+
 
     def clear(self) -> None:
         """
@@ -42,4 +57,6 @@ class Stack:
 
         :return: None
         """
+
+        self.__my_stack.clear()
         return None
