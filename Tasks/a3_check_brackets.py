419a52048b5200cabce6162d98de85b179496df5
EvgeniiRomanov <e78rus@mail.ru>
2022-02-18 16:41:34 +0000

    Решил домашку со скобками - 2 варианта 1. Со счетчиками 2. Просто стэком

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,33 +5,56 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+    # -------------- Вариант со счетчиками -----------------------
+    # brackets_open = "("
+    # brackets_closed = ")"
+    #
+    # stack = []                  # стэк для входа-выхода скобок
+    # count_in = 0                # счётчик входных скобок
+    # count_out = 0               # счётчик выходных скобок
+    #
+    # for index, elem in enumerate(brackets_row):
+    #     if elem in brackets_open:
+    #         count_in += 1
+    #         stack.append(elem)
+    #         print(f"Вход: {stack}, {count_in}")
+    #     if elem in brackets_closed:
+    #         count_out += 1
+    #         print(f"Выходят: {stack}, вх:{count_in}, вых: {count_out}")
+    #         if len(stack) == 0:
+    #             return False
+    #         elif count_out > count_in:
+    #             return False
+    #         stack.pop()
+    #         count_in -= 1
+    #         count_out -= 1
+    #
+    #     print(f"Прошли шаг: {stack}, вх:{count_in}, вых: {count_out}")
+    #     if index == len(brackets_row) - 1:
+    #         if len(stack) > 0:
+    #             return False
+    #
+    # return True
+
+# -------------------- Вариант просто стеком ------------------------
     brackets_open = "("
     brackets_closed = ")"
 
     stack = []                  # стэк для входа-выхода скобок
-    count_in = 0                # счётчик входных скобок
-    count_out = 0               # счётчик выходных скобок
 
     for index, elem in enumerate(brackets_row):
         if elem in brackets_open:
-            count_in += 1
             stack.append(elem)
-            print(f"Вход: {stack}, {count_in}")
+            #print(f"Вход: {len(stack)}")
         if elem in brackets_closed:
-            count_out += 1
-            print(f"Выходят: {stack}, вх:{count_in}, вых: {count_out}")
+            #print(f"Выходят: {len(stack)}")
             if len(stack) == 0:
                 return False
-            elif count_out > count_in:
-                return False
             stack.pop()
-            count_in -= 1
-            count_out -= 1
 
-        print(f"Прошли шаг: {stack}, вх:{count_in}, вых: {count_out}")
+        #print(f"Прошли шаг - длина: {len(stack)}")
         if index == len(brackets_row) - 1:
             if len(stack) > 0:
                 return False
 
     return True
-
