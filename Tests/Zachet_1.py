68234d0e1c3b2f114895334bf0ff4bb8b933d9ea
EvgeniiRomanov <e78rus@mail.ru>
2022-03-17 15:13:56 +0000

    Zachet1 - задача с ракетой тесты отредактированы

--- Tests/Zachet_1.py
+++ Tests/Zachet_1.py
@@ -1,22 +1,22 @@
-# подправить тест на решение задачи вхождением разных комбинаций
-
 import unittest
-from Tasks.a3_check_brackets import check_brackets
+from Tasks.Zachet_1 import rocket_rent
 
 
 class MyTestCase(unittest.TestCase):
     def test_invalid(self):
-        self.assertFalse(check_brackets("(()("))
-        self.assertFalse(check_brackets("(()))"))
-        self.assertFalse(check_brackets("(()))"))
-        self.assertFalse(check_brackets(")("))
-        self.assertFalse(check_brackets(")()("))
+        self.assertFalse(rocket_rent([(0, 12), (2, 6)]))
+        self.assertFalse(rocket_rent([(0, 12), (12, 15), (14, 24)]))
+        self.assertFalse(rocket_rent([(10, 12), (12, 15), (17, 19), (19, 23), (7, 10), (16, 17), (13, 14), (18, 23)]))
+        self.assertFalse(rocket_rent([(22, 24), (12, 15), (14, 24)]))
 
     def test_valid(self):
-        self.assertTrue(check_brackets(""))
-        self.assertTrue(check_brackets("()()"))
-        self.assertTrue(check_brackets("(()())"))
-        self.assertTrue(check_brackets("((()(())()))()"))
+        self.assertTrue(rocket_rent([(12, 15), (19, 23), (7, 10), (16, 17)]))
+        self.assertTrue(rocket_rent([(10, 12), (12, 15), (17, 19), (19, 23), (7, 10), (16, 17), (0, 6)]))
+        self.assertTrue(rocket_rent([(0, 12), (12, 15), (15, 24)]))
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 if __name__ == '__main__':
